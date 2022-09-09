abfbacd0b996fb75073248a656cc762b87788dfc
Kalpesh Somwanshi <86122757+Kalpesh910@users.noreply.github.com>
2022-09-09 18:25:53 +0530

    Create 239. Sliding Window Maximum.cpp
    dequeue concept has learned from this question
    in dequeue we can add elements by front as well as rear
    remove elements from front as well as rear
    https://www.geeksforgeeks.org/deque-cpp-stl/

---
+++ 239. Sliding Window Maximum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int i=0;
+        deque<int>qu;
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            if(!qu.empty() && qu.front()==i-k)    qu.pop_front();
+            while(!qu.empty() && nums[qu.back()]<nums[i]){
+                qu.pop_back();
+            }
+            qu.push_back(i);
+            if(i>=k-1)  ans.push_back(nums[qu.front()]);
+        }
+        return ans;
+    }
+};
